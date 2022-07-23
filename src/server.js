aca50e44ef86f8a704fc41b5b372789db96c01b0
Yugo <yugosakamoto0717@gmail.com>
2022-07-22 23:21:54 -0300

    [hotfix] Corrects environment variables for dotenv

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import categoriesRouter from "./routers/categoriesRouter.js";
 import gamesRouter from "./routers/gamesRouter.js";
 import customersRouter from "./routers/customersRouter.js";
 import rentalsRouter from "./routers/rentalsRouter.js";
 
 const server = express();
+dotenv.config();
+const PORT = process.env.PORT || 4000;
 
 server.use(cors());
 server.use(express.json());
@@ -15,4 +18,4 @@ server.use(gamesRouter);
 server.use(customersRouter);
 server.use(rentalsRouter);
 
-server.listen(4000, () => console.log("Servidor rodando na porta 4000."));
+server.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
