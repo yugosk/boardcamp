fe27477ba3b1f2649a49fec373c5aaeb88668e19
Yugo <yugosakamoto0717@gmail.com>
2022-07-25 16:47:58 -0300

    [hotfix] Fixes customerSchema

--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -2,7 +2,14 @@ import Joi from "joi";
 
 export const customerSchema = Joi.object({
   name: Joi.string().required(),
-  phone: Joi.string().min(10).max(11).required(),
-  cpf: Joi.string().length(11).required(),
+  phone: Joi.string()
+    .min(10)
+    .max(11)
+    .pattern(/\d{11}/)
+    .required(),
+  cpf: Joi.string()
+    .length(11)
+    .pattern(/\d{11}/)
+    .required(),
   birthday: Joi.date().required(),
 });
